9bfdd539b602dbea710f0f14a0cd928706dc23ce
Daniel Frederick <danandabbey502@gmail.com>
2024-12-09 16:46:34 -0500

    Add Podcast and Placeholder variants to Media enum; implement get_by_index method in Catalog

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,80 @@
 #[derive(Debug)]
-enum Media{
-    Book {title: String, author: String},
-    Movie {title: String, director: String},
-    Audiobook {title: String},
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String },
+    Podcast(u32),
+    Placeholder,
 }
 
 impl Media {
-    fn print(&self){
-        print!("{:#?}",self)
+    fn print(&self) {
+        print!("{:#?}", self)
     }
 
-    fn description(&self)->String{
-        match self{
-            Media::Book{title,author} =>{
-                format!("Book: {},{}",title,author)
+    fn description(&self) -> String {
+        match self {
+            Media::Book { title, author } => {
+                format!("Book: {},{}", title, author)
             }
-            Media::Movie{title,director} =>{
-                format!("Movie: {},{}",title,director)
+            Media::Movie { title, director } => {
+                format!("Movie: {},{}", title, director)
             }
-            Media::Audiobook{title} =>{
-                format!("Audiobook: {}",title)
+            Media::Audiobook { title } => {
+                format!("Audiobook: {}", title)
+            }
+            Media::Podcast(ep) => {
+                format!("Podcast: {}", ep)
+            }
+            Media::Placeholder => {
+                format!("Placeholder")
             }
         }
-    } 
+    }
 }
 #[derive(Debug)]
-struct Catalog{
+struct Catalog {
     items: Vec<Media>,
 }
 
-impl Catalog{
-    fn new() -> Self{
-        Catalog{ items: vec![]}
+impl Catalog {
+    fn new() -> Self {
+        Catalog { items: vec![] }
     }
 
-    fn add(&mut self, media: Media){
+    fn add(&mut self, media: Media) {
         self.items.push(media);
+    }
+    fn get_by_index(&self, index: usize) -> Option<&Media> {
+        if self.items.len() > index {
+            Some(&self.items[index])
+        } else {
+            None
+        }
     }
 }
 
 fn main() {
-    let audiobook = Media::Audiobook { title: String::from("dan") };
-    println!("{}",audiobook.description())
+    let audiobook = Media::Audiobook {
+        title: String::from("dan"),
+    };
+    let podcast = Media::Podcast(10);
+    let placeholder = Media::Placeholder;
+
+    let mut catalog = Catalog::new();
+
+    catalog.add(audiobook);
+    catalog.add(podcast);
+    catalog.add(placeholder);
+
+    let item = catalog.get_by_index(40);
+
+    match item {
+        Some(value) => {
+            println!("Item: {:#?}", value)
+        }
+        None => {
+            println!("No item at that index.")
+        }
+    }
 }
